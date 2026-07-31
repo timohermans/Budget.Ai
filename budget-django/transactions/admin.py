a3a59bfaf6ce2f3c5e34f4ee6ab0c8efbb4597d0
Timo Hermans <tmhermans@gmail.com>
2026-07-31 21:23:37 +0200

    chore: Remove Django app

--- budget-django/transactions/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from transactions.models import Transaction
-
-admin.site.register(Transaction)
--- budget-django/user/type.py
+++
@@ -1,8 +0,0 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from django.contrib.auth.models import AbstractUser
-    User = AbstractUser
-else:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
